bb63199a1c59a2f7e4e91e30f23f9d2f9d2781a0
cahsemprul <cahsemprul@localhost.localdomain>
2020-08-28 10:50:14 +0700

    Convert HTML to svelte components

---
+++ src/data/charities.js
@@ -0,0 +1,16 @@
+module.exports = {
+    charities: [
+        {
+            id: 1,
+            title: 'First Charity Project',
+            category: 'Money',
+            thumbnail: 'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
+            pledged: 99000,
+            target: 100000,
+            date_end: +new Date('10 September 2020'),
+            profile_photo: 'https://live.staticflickr.com/7721/17102022497_84b7c9c342_w.jpg',
+            profile_name: 'Viroide Bueno',
+            no_pledges: 0
+        }
+    ]
+}
