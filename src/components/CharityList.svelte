1246e4da86d6789e4e40db2e74df17b37c4b689f
cahsemprul <cahsemprul@localhost.localdomain>
2020-08-28 21:48:39 +0700

    SPA Feature

--- src/components/CharityList.svelte
+++ src/components/CharityList.svelte
@@ -141,10 +141,11 @@
 
                     <span class="xs-separetor"></span>
 
-                    <button on:click={handleButton} data-toggle="modal" data-target="#exampleModal"
+                    <a href="/donation/{charity.id}" data-toggle="modal"
+                    data-target="#exampleModal"
                     class="btn btn-primary btn-block">
                     Donate This Cause
-                    </button>
+                    </a>
                     </div><!-- .xs-item-content END -->
                 </div><!-- .xs-popular-item END -->
                 </div>
